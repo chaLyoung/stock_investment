452779a18d2f80f60a4acf765b25febf5b420c69
chaLyoung <dreamlovinghigh57@gmail.com>
2022-10-05 21:43:40 +0900

    stock dashin

--- main.py
+++ main.py
@@ -21,6 +21,39 @@ class StockStart(QWidget):
         super().__init__()
         self.start()
 
+    def start(self):
+        global getCodeListBtn
+
+    def start(self):
+        global getCodeListBtn
+
+        self.setWindowTitle('Kiwoom Stock Investment')
+        self.setFixedSize(400, 250)
+        self.setFocusPolicy(Qt.StrongFocus)
+
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom.dynamicCall("CommConnect()")
+
+        self.kiwoom.OnReceiveTrData.connect(self.receive_trdata)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+
+        getCodeListBtn = QtWidgets.QPushButton('Get Code List')
+        layout.addWidget(getCodeListBtn)
+
+        self.setLayout(layout)
+        self.show()
+
+        self.kiwoom.OnEventConnect.connect(self.checkStatus)
+
+    def checkStatus(self, err_code):
+        global getCodeListBtn
+
+        if err_code == 0:
+            getCodeListBtn.clicked.connect(self.getCodeList)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = StockStart()
